5e472a7d4b96c04d33c506ae3321d58676e3867b
yodatk <yodatk@gmail.com>
2018-09-30 23:48:42 +0300

    Knight calculateAllPossibleMoves finished and tested --> approved

--- src/ChessGame/Logic/Pieces/Knight.java
+++ src/ChessGame/Logic/Pieces/Knight.java
@@ -3,16 +3,84 @@ package ChessGame.Logic.Pieces;
 import ChessGame.Logic.Column;
 import ChessGame.Logic.Row;
 import ChessGame.Logic.Tile;
+import com.sun.xml.internal.bind.v2.TODO;
 import javafx.util.Pair;
 
-public class Knight extends Piece{
+import java.util.HashSet;
+
+public class Knight extends Piece {
     public Knight(Color pieceColor, Pair<Column, Row> coordinate) {
         super(pieceColor, coordinate);
     }
 
     @Override
     public void calculateAllPossibleMoves(Tile[][] currentBoard) {
+        //resetting the possibleMoves
+        this.possibleMoves = new HashSet<>();
+        addUp(currentBoard);
+        addDown(currentBoard);
+        addLeft(currentBoard);
+        addRight(currentBoard);
+    }
 
+    private void addUp(Tile[][] currentBoard) {
+        //checking the checking up-left
+        Pair<Column, Row> nextLeft = new Pair<>(this.coordinate.getKey().getPrevious(), this.coordinate.getValue().getNext());
+        if ((nextLeft.getKey() != null) && (nextLeft.getValue() != null) && nextLeft.getValue().getNext() != null) {
+            nextLeft = new Pair<>(nextLeft.getKey(), nextLeft.getValue().getNext());
+            checkForPieces(this.possibleMoves, nextLeft, currentBoard);
+        }
+        //checking the up-right
+        Pair<Column, Row> nextRight = new Pair<>(this.coordinate.getKey().getNext(), this.coordinate.getValue().getNext());
+        if ((nextRight.getKey() != null) && (nextRight.getValue() != null) && nextRight.getValue().getNext() != null) {
+            nextRight = new Pair<>(nextRight.getKey(), nextRight.getValue().getNext());
+            checkForPieces(this.possibleMoves, nextRight, currentBoard);
+        }
+    }
+
+    private void addDown(Tile[][] currentBoard) {
+        //checking the down-left
+        Pair<Column, Row> nextLeft = new Pair<>(this.coordinate.getKey().getPrevious(), this.coordinate.getValue().getPrevious());
+        if ((nextLeft.getKey() != null) && (nextLeft.getValue() != null) && (nextLeft.getValue().getPrevious() != null)) {
+            nextLeft = new Pair<>(nextLeft.getKey(), nextLeft.getValue().getPrevious());
+            checkForPieces(this.possibleMoves, nextLeft, currentBoard);
+        }
+        //checking the down-right
+        Pair<Column, Row> nextRight = new Pair<>(this.coordinate.getKey().getNext(), this.coordinate.getValue().getPrevious());
+        if ((nextRight.getKey() != null) && (nextRight.getValue() != null) && nextRight.getValue().getPrevious() != null) {
+            nextRight = new Pair<>(nextRight.getKey(), nextRight.getValue().getPrevious());
+            checkForPieces(this.possibleMoves, nextRight, currentBoard);
+        }
+    }
+
+    private void addLeft(Tile[][] currentBoard) {
+        //checking the Left-Up
+        Pair<Column, Row> nextUp = new Pair<>(this.coordinate.getKey().getPrevious(), this.coordinate.getValue().getNext());
+        if ((nextUp.getKey() != null) && (nextUp.getValue() != null) && nextUp.getKey().getPrevious() != null) {
+            nextUp = new Pair<>(nextUp.getKey().getPrevious(), nextUp.getValue());
+            checkForPieces(this.possibleMoves, nextUp, currentBoard);
+        }
+        //checking the Left-Down
+        Pair<Column, Row> nextDown = new Pair<>(this.coordinate.getKey().getPrevious(), this.coordinate.getValue().getPrevious());
+        if ((nextDown.getKey() != null) && (nextDown.getValue() != null) && (nextDown.getKey().getPrevious() != null)) {
+            nextDown = new Pair<>(nextDown.getKey().getPrevious(), nextDown.getValue());
+            checkForPieces(this.possibleMoves, nextDown, currentBoard);
+        }
+    }
+
+    private void addRight(Tile[][] currentBoard) {
+        //checking the Right-Up
+        Pair<Column, Row> nextUp = new Pair<>(this.coordinate.getKey().getNext(), this.coordinate.getValue().getNext());
+        if ((nextUp.getKey() != null) && (nextUp.getValue() != null) && nextUp.getKey().getNext() != null) {
+            nextUp = new Pair<>(nextUp.getKey().getNext(), nextUp.getValue());
+            checkForPieces(this.possibleMoves, nextUp, currentBoard);
+        }
+        //checking the Right-Down
+        Pair<Column, Row> nextDown = new Pair<>(this.coordinate.getKey().getNext(), this.coordinate.getValue().getPrevious());
+        if ((nextDown.getKey() != null) && (nextDown.getValue() != null) && (nextDown.getKey().getNext()) != null) {
+            nextDown = new Pair<>(nextDown.getKey().getNext(), nextDown.getValue());
+            checkForPieces(this.possibleMoves, nextDown, currentBoard);
+        }
     }
 
     @Override
--- src/ChessGame/Logic/Pieces/KnightTest.java
+++ src/ChessGame/Logic/Pieces/KnightTest.java
@@ -1,9 +1,12 @@
 package ChessGame.Logic.Pieces;
 
-import ChessGame.Logic.Column;
-import ChessGame.Logic.Row;
+import ChessGame.Logic.*;
 import javafx.util.Pair;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -14,9 +17,115 @@ public class KnightTest {
     private Knight equ4 = new Knight(Piece.Color.WHITE,new Pair<>(Column.D, Row.ONE));
     private Knight equ5 = new Knight(Piece.Color.BLACK,new Pair<>(Column.E, Row.FIVE));
     private Knight equ6 = new Knight(Piece.Color.WHITE,new Pair<>(Column.F, Row.SEVEN));
+
+    private Tile[][] currentBoard;
+
+    private void setemptyboard1(){
+        Board board = new Board();
+        currentBoard = board.getTiles();
+
+        currentBoard[0][0].setCurrentPiece(null);
+        currentBoard[0][1].setCurrentPiece(null);
+        currentBoard[0][2].setCurrentPiece(null);
+        currentBoard[0][3].setCurrentPiece(null);
+        currentBoard[0][4].setCurrentPiece(null);
+        currentBoard[0][5].setCurrentPiece(null);
+        currentBoard[0][6].setCurrentPiece(null);
+        currentBoard[0][7].setCurrentPiece(null);
+
+        currentBoard[1][0].setCurrentPiece(null);
+        currentBoard[1][1].setCurrentPiece(null);
+        currentBoard[1][2].setCurrentPiece(null);
+        currentBoard[1][3].setCurrentPiece(null);
+        currentBoard[1][4].setCurrentPiece(null);
+        currentBoard[1][5].setCurrentPiece(null);
+        currentBoard[1][6].setCurrentPiece(null);
+        currentBoard[1][7].setCurrentPiece(null);
+
+        currentBoard[6][0].setCurrentPiece(null);
+        currentBoard[6][1].setCurrentPiece(null);
+        currentBoard[6][2].setCurrentPiece(null);
+        currentBoard[6][3].setCurrentPiece(null);
+        currentBoard[6][4].setCurrentPiece(null);
+        currentBoard[6][5].setCurrentPiece(null);
+        currentBoard[6][6].setCurrentPiece(null);
+        currentBoard[6][7].setCurrentPiece(null);
+
+
+        currentBoard[7][0].setCurrentPiece(null);
+        currentBoard[7][1].setCurrentPiece(null);
+        currentBoard[7][2].setCurrentPiece(null);
+        currentBoard[7][3].setCurrentPiece(null);
+        currentBoard[7][4].setCurrentPiece(null);
+        currentBoard[7][5].setCurrentPiece(null);
+        currentBoard[7][6].setCurrentPiece(null);
+        currentBoard[7][7].setCurrentPiece(null);
+
+    }
+
     @Test
     public void calculateAllPossibleMoves() {
-        fail("Test not Written yet!");
+        System.out.println("calculateAllPossibleMoves Test has started");
+        setemptyboard1();
+        Set<Pair<Column,Row>> possibleMoves1 = new HashSet(Arrays.asList(
+                Coordinates.C7,Coordinates.E7,
+                Coordinates.B6,Coordinates.F6,
+                Coordinates.B4,Coordinates.F4,
+                Coordinates.C3,Coordinates.E3
+        ));
+        Set<Pair<Column,Row>> possibleMoves2 = new HashSet(Arrays.asList(
+                Coordinates.B6,Coordinates.C7
+        ));
+        Set<Pair<Column,Row>> possibleMoves3 = new HashSet(Arrays.asList(
+                Coordinates.C7
+        ));
+        Set<Pair<Column,Row>> possibleMoves4 = new HashSet(Arrays.asList(
+                Coordinates.E7,
+                Coordinates.B6,Coordinates.F6,
+                Coordinates.B4,
+                Coordinates.C3,Coordinates.E3
+        ));
+        //queen on d5 without obstacles
+        Knight toTest = new Knight(Piece.Color.WHITE,Coordinates.D5);
+        currentBoard[3][3].setCurrentPiece(toTest);
+        toTest.calculateAllPossibleMoves(currentBoard);
+        Set<Pair<Column,Row>> results = toTest.getPossibleMoves();
+        assertEquals("Error --> when Knight on D5",possibleMoves1, results);
+        System.out.println("Knight on D5 passed");
+        currentBoard[3][3].setCurrentPiece(null);
+        //queen on a8 without obstacles
+        toTest.setCoordinate(Coordinates.A8);
+        currentBoard[0][0].setCurrentPiece(toTest);
+        toTest.calculateAllPossibleMoves(currentBoard);
+        results = toTest.getPossibleMoves();
+        assertEquals("Error --> when Knight on A8",possibleMoves2, results);
+        System.out.println("Knight on A8 passed");
+        //queen on a8 with obstacles
+        currentBoard[1][2].setCurrentPiece(new Rook(Piece.Color.BLACK,Coordinates.C7));
+        currentBoard[1][1].setCurrentPiece(new Rook(Piece.Color.WHITE,Coordinates.B7));
+        currentBoard[2][1].setCurrentPiece(new Rook(Piece.Color.WHITE,Coordinates.B6));
+        toTest.calculateAllPossibleMoves(currentBoard);
+        results = toTest.getPossibleMoves();
+        assertEquals("Error --> when Knight on A8 with obstacles",possibleMoves3, results);
+        System.out.println("Knight on A8 with obstacles passed");
+        currentBoard[1][0].setCurrentPiece(null);
+        currentBoard[0][1].setCurrentPiece(null);
+        currentBoard[2][1].setCurrentPiece(null);
+        currentBoard[0][0].setCurrentPiece(null);
+        toTest.setCoordinate(Coordinates.D5);
+        currentBoard[3][3].setCurrentPiece(toTest);
+
+        currentBoard[2][1].setCurrentPiece(new Pawn(Piece.Color.BLACK, Coordinates.B6));
+        currentBoard[1][2].setCurrentPiece(new Pawn(Piece.Color.WHITE, Coordinates.C7));
+        currentBoard[4][5].setCurrentPiece(new Pawn(Piece.Color.WHITE, Coordinates.F4));
+        currentBoard[5][3].setCurrentPiece(new Pawn(Piece.Color.BLACK, Coordinates.E3));
+
+        toTest.calculateAllPossibleMoves(currentBoard);
+        results = toTest.getPossibleMoves();
+        assertEquals("Error --> when Knight on D5 with obstacles",possibleMoves4, results);
+        System.out.println("Knight on D5 with obstacles passed");
+
+
 
     }
 
